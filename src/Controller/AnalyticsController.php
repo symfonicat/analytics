2da7b35b45bc76edaf9f4cb9bff8be5ca3a9166b
tayp <dharkflower@gmail.com>
2026-04-30 21:09:20 -0600

    rebase

---
+++ src/Controller/AnalyticsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Symfonicat\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/m/analytics')]
+final class AnalyticsController extends AbstractModuleController
+{
+    #[Route('', name: 'symfonicat_module_analytics', methods: ['POST'])]
+    public function index(): Response
+    {
+        return $this->module(new JsonResponse([
+            'working' => true,
+        ]));
+    }
+}
