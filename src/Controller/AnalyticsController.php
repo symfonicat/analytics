936dae7e114888c8664a25d9aefebd9bd45ed1e6
tayp <dharkflower@gmail.com>
2026-05-27 14:11:44 -0600

    module json

--- src/Controller/AnalyticsController.php
+++ src/Controller/AnalyticsController.php
@@ -4,14 +4,22 @@ namespace Symfonicat\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/m/symfonicat/analytics')]
 final class AnalyticsController extends AbstractModuleController
 {
+
     #[Route('/main', name: 'module_symfonicat_analytics_main', methods: ['POST'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $moduleJson = $request->attributes->get('module_json');
+
+        $output = print_r($moduleJson, true);
+        $this->logger->info("module_json OUTPUT = $output");
+
         return $this->module(new JsonResponse([
             'working' => true,
         ]));
